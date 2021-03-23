c81ee9b1c859bfbb8be8f2554bd16e270dfcbefb
欧援 <ouyuan@toponad.com>
2021-03-23 15:31:32 +0800

    feat: 初始化客户端

---
+++ client/src/pages/about/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function About () {
+  return (
+    <div className='about-page__container'>关于我们</div>
+  )
+}
+
+export default About
---
+++ client/src/pages/manager/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Manager () {
+  return (
+    <div className='manager-page__container'>管理页面</div>
+  )
+}
+
+export default Manager
