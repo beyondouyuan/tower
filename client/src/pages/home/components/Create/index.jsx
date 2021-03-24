6aba50a5d9123e9f52a9c66cecdbe8fb0d549767
欧援 <ouyuan@toponad.com>
2021-03-24 16:33:33 +0800

    feat: 引入antd

--- client/src/pages/home/components/Create/index.jsx
+++ client/src/pages/home/components/Create/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button } from 'antd'
 
 import styles from './style.module.scss'
 
@@ -6,6 +7,7 @@ function Create (props) {
   return (
     <div className={styles['create-container']}>
       <div className={styles['create-container__body']}>
+        <Button type="primary">Button</Button>
         <div className={styles['btn']} onClick={() => props.onPress()}>立刻创建</div>
       </div>
     </div>
